7087b52bf8a4a135373d545284f9f7ea73b6cafc
Aesgarth <getflair@gmail.com>
2025-01-04 11:19:37 +0000

    Update run.sh

--- grocy_scanner/run.sh
+++ grocy_scanner/run.sh
@@ -3,13 +3,13 @@
 # Log add-on startup
 bashio::log.info "Starting Grocy Scanner Add-on"
 
-# Retrieve options from config.yaml
+# Retrieve options from the configuration
 API_KEY=$(bashio::config 'api_key')
 GROCY_URL=$(bashio::config 'grocy_url')
 
-# Log configuration values (optional, redact sensitive info in production)
-bashio::log.info "Using API Key: ${API_KEY}"
+# Log configuration
+bashio::log.info "API Key: ${API_KEY}"
 bashio::log.info "Grocy URL: ${GROCY_URL}"
 
-# Run the .NET application
-exec dotnet GrocyScanner.Service.dll
+# Start the .NET application
+exec dotnet /app/GrocyScanner.Service.dll
