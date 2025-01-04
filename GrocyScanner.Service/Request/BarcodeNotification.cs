3ab13d2311ff778786e4f2868fb4b190e0d9d532
Aesgarth <getflair@gmail.com>
2025-01-04 09:37:41 +0000

    Delete GrocyScanner.Service directory

--- GrocyScanner.Service/Request/BarcodeNotification.cs
+++
@@ -1,6 +0,0 @@
-namespace GrocyScanner.Service.Request;
-
-public class BarcodeNotification
-{
-    public required string Gtin { get; set; }
-}
--- GrocyScanner.Service/Request/CameraNotification.cs
+++
@@ -1,8 +0,0 @@
-namespace GrocyScanner.Service.Request;
-
-public class CameraNotification
-{
-    public required string Id { get; set; }
-    
-    public required string Label { get; set; }
-}
